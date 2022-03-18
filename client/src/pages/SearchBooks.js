022611402d5e10aad277a53e66eec57218864b41
Evan Bulliner <evan_bulliner@yahoo.com>
2022-03-17 23:05:27 -0500

    fixed minor coding issue. hopefully this will solve deployment issue and saving the books.

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
-import Auth from '../utils/auth';
-import { searchGoogleBooks } from '../utils/API';
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-
+import { searchGoogleBooks } from "../utils/API";
 import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
+
+import Auth from '../utils/auth';
+import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -14,11 +13,10 @@ const SearchBooks = () => {
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
 
-  // mutation to saveBook
-  const [saveBook] = useMutation(SAVE_BOOK);
-
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [ saveBook ] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -62,7 +60,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
+    
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -72,11 +70,10 @@ const SearchBooks = () => {
 
     try {
       await saveBook({
-        variables: { input: bookToSave}
+        variables: { book: bookToSave }
       });
 
-      // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+      setSavedBookIds([ ...savedBookIds, bookToSave.bookId ]);
     } catch (err) {
       console.error(err);
     }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -14,6 +14,7 @@ query Query {
       description
       image
       title
+      link
     }
   }
 }
